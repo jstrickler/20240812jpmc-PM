09acb203bd33a1307c1600d9ec6434da8dcc8cf2
John Strickler <jstrickler@gmail.com>
2024-08-15 18:44:34 +0000

    Thursday 2:45

--- EXAMPLES/use_geometry_3.py
+++ EXAMPLES/use_geometry_3.py
@@ -1,6 +1,11 @@
-from geometry import circle_area, rectangle_area, square_area
+from EXAMPLES.alpha.mathlib.geometry import circle_area   #, rectangle_area, square_area
+import EXAMPLES.alpha.mathlib.geometry as geometry
 
 a1 = circle_area(8)
-a2 = rectangle_area(10, 12)
-a3 = square_area(7.9)
-print(a1, a2, a3)
+print(f"{a1 = }")
+a2 = g.circle_area(8)
+a3 = geometry.circle_area(8)
+
+# a2 = rectangle_area(10, 12)
+# a3 = square_area(7.9)
+# print(a1, a2, a3)
